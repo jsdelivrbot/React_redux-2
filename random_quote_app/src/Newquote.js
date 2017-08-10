d6d0d847a5183f76fd9a0e1d7a87a3e02f36cc47
luna <lunaceee@gmail.com>
2017-08-10 15:09:43 -0700

    added random quote app

---
+++ random_quote_app/src/Newquote.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Newquote extends React.Component {
+	render(){
+		return(
+			<button className="new-quote">
+				more
+			</button>
+		)
+	}
+}
+
+export default Newquote;
---
+++ random_quote_app/src/Quote.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import Newquote from './Newquote.js';
+
+
+var myHeaders = new Headers();//create new headers.
+myHeaders.append("Accept", "application/json");//customize header.
+
+// Create init 
+var myInit = {
+        method: 'GET',
+        headers: myHeaders,
+        mode: 'cors',
+        cache: 'default'
+      };
+
+// Clone request.
+var myRequest = new Request('https://icanhazdadjoke.com/', myInit);
+
+class Quote extends React.Component {
+	constructor(props) {
+    super(props);
+    this.state = {quote: 'Hello!'};
+    // Bind "this" to handleClick event.
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+	// Fetch data from API.
+	fetchQuote() {
+    var t = this;
+    fetch(myRequest)
+    .then((response) => {
+      return response.json();
+    })
+    .then((obj) => {
+      t.setState({
+        quote: obj.joke
+      })
+    })
+  }
+
+  // Mount the component.
+  componentDidMount(){
+    this.fetchQuote();
+  }
+
+  // Helper function to handle click event.
+  handleClick() {
+    this.fetchQuote();
+  }
+
+	render(){
+		return (
+			<div className="quote-content">
+        <h2>{this.state.quote}</h2>
+        <div className="tweet-btn">
+					<a href={'https://twitter.com/intent/tweet?text=' + this.state.quote} target="_blank">
+					Tweet
+					</a>
+				</div>
+        <Newquote onClick={this.handleClick}/>
+      </div>
+		);
+	}
+}
+
+export default Quote;
