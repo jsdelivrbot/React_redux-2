3f122c603088a4828c6c678b46f4997fed012b42
luna <lunaceee@gmail.com>
2017-08-10 16:07:26 -0700

    fixed indentation

--- random_quote_app/src/Newquote.js
+++ random_quote_app/src/Newquote.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
 class Newquote extends React.Component {
-	render(){
-		console.log(this.props.foo);
-		console.log(this.props.onClick);
-		return(
-			<button onClick={this.props.handleClick} className="new-quote">
-				more
-			</button>
-		)
-	}
+  render(){
+    console.log(this.props.foo);
+    console.log(this.props.onClick);
+    return(
+      <button onClick={this.props.handleClick} className="new-quote">
+        more
+      </button>
+    )
+  }
 }
 
 export default Newquote;
